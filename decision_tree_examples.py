85bbd6e59895815a4d62ffcd1f9d2b325656c494
Nathan Gilbert <nathan.gilbert@gmail.com>
2021-02-17 19:04:19 -0700

    string plotting

--- decision_tree_examples.py
+++ decision_tree_examples.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import LabelBinarizer
+from sklearn import tree
 
 
 if __name__ == "__main__":
@@ -11,9 +12,8 @@ if __name__ == "__main__":
     # easily pull in csv data with pandas dataframes
     dataset = pd.read_csv(input_file)
 
-    print(dataset.head())
-
-    print(dataset.describe())
+    # print(dataset.head())
+    # print(dataset.describe())
 
     X = dataset.drop(['name', 'type', 'rating'], axis=1)
 
@@ -23,11 +23,11 @@ if __name__ == "__main__":
     ohe_df = pd.DataFrame(transformed)
     X = pd.concat([X, ohe_df], axis=1).drop(['mfr'], axis=1)
 
-    print(X.head())
-    print(X.dtypes)
+    # print(X.head())
+    # print(X.dtypes)
 
     y = dataset['rating']
-    print(y.head())
+    # print(y.head())
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -36,4 +36,15 @@ if __name__ == "__main__":
     y_pred = dt.predict(X_test)
 
     df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-    print(df)
+    # print(df)
+
+    mfr_names = ['American Home Food Products',
+                 'General Mills',
+                 'Kelloggs',
+                 'Nabisco',
+                 'Post',
+                 'Quaker Oats',
+                 'Ralston Purina']
+    feature_names = list(X.columns.values)[:-7] + mfr_names
+    text_representation = tree.export_text(dt, feature_names=feature_names)
+    print(text_representation)
